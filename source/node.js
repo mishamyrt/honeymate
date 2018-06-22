a9431cf95e8d7af43ab99484073b3cb0acff865f
Misha <misha@myrt.co>
2018-06-22 13:07:50 +0300

    Short literal return

--- source/node.js
+++ source/node.js
@@ -13,23 +13,21 @@ const getDirection = (dataset) => {
     }
 }
 
-const parseParameters = (dataset) => {
-    return {
-        direction: getDirection(dataset),
-        duration: dataset.duration ? dataset.duration : 640,
-        effect: dataset.effect ? dataset.effect : 'fade',
-        expose: dataset.expose ? dataset.expose === 'true' : false,
-        delay: dataset.delay ? parseInt(dataset.delay, 10) : 0,
-        hold: dataset.hold ? parseInt(dataset.hold, 10) : 0,
-        scale: dataset.scale ? dataset.scale : 0.87,
-        await: dataset.await ? dataset.await : null,
-        'continue': dataset.continue === 'true',
-        origin: dataset.origin ? dataset.origin : 'bottom',
-        offset: dataset.up || dataset.down || dataset.left || dataset.right
-            ? dataset.up || dataset.down || dataset.left || dataset.right
-            : 32,
-    }
-}
+const parseParameters = (dataset) => ({
+    direction: getDirection(dataset),
+    duration: dataset.duration ? dataset.duration : 640,
+    effect: dataset.effect ? dataset.effect : 'fade',
+    expose: dataset.expose ? dataset.expose === 'true' : false,
+    delay: dataset.delay ? parseInt(dataset.delay, 10) : 0,
+    hold: dataset.hold ? parseInt(dataset.hold, 10) : 0,
+    scale: dataset.scale ? dataset.scale : 0.87,
+    await: dataset.await ? dataset.await : null,
+    'continue': dataset.continue === 'true',
+    origin: dataset.origin ? dataset.origin : 'bottom',
+    offset: dataset.up || dataset.down || dataset.left || dataset.right
+        ? dataset.up || dataset.down || dataset.left || dataset.right
+        : 32,
+})
 
 export default class HoneyNode {
     constructor(node) {
