87aee79662a03c42007dc93b8612b6bf729e77f5
Misha <misha@myrt.co>
2018-06-23 00:21:27 +0300

    server

---
+++ test/index.js
@@ -0,0 +1,18 @@
+/* eslint-env node */
+/* eslint no-console: 0 */
+
+const staticServer = require('node-static')
+const http = require('http')
+
+const handler = new staticServer.Server('test/')
+const PORT = 1337
+
+http.createServer((request, response) => {
+    if (request.url === '/honeymate') {
+        handler.serveFile('../dist/honeymate.js', 200, {}, request, response)
+    } else {
+        handler.serve(request, response)
+    }
+}).listen(PORT)
+
+console.log('Server is listening on 127.0.0.1:' + PORT)
