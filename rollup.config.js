22909d144f9fd9d39d801ba8a7e00b4bef7c72ab
mishamyrt <misha@myrt.co>
2018-10-22 19:52:02 +0300

    Improved build for develop

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,28 @@
 import babel from 'rollup-plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 
+/* eslint-env node */
+
+const production = process.env.prod || false
+
 export default {
     input: 'source/honeymate.js',
     output: {
-        file: 'dist/honeymate.js',
+        file: production ? 'dist/honeymate.js' : 'build/honeymate.js',
         format: 'iife',
     },
     plugins: [
         babel({
             exclude: 'node_modules/**',
-            presets: [['env', { modules: false }]],
+            presets: [
+                ['env', {
+                    modules: false,
+                    useBuiltIns: 'entry',
+                }],
+            ],
             plugins: ['external-helpers'],
             externalHelpers: true,
         }),
-        terser(),
+        production ? terser() : false,
     ],
 }
