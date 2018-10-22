07767a6405c50da87e83e26fc95c725f6c13c8a6
mishamyrt <misha@myrt.co>
2018-10-22 21:04:13 +0300

    First commit

--- source/honeymate.js
+++
@@ -1,8 +0,0 @@
-import Honeymate from './index.js'
-
-document.addEventListener('DOMContentLoaded', () => {
-    const style = document.createElement('style')
-    style.innerHTML = '.honey{opacity:0}'
-    document.head.appendChild(style)
-    Honeymate.initiate()
-})
