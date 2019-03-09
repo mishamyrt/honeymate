fbc0a0cb0cc625d508ef06f434c8d10a0cf55be6
mishamyrt <misha@myrt.co>
2019-03-09 11:24:57 +0300

    Updated tests architecture, added Page class

--- test/index.js
+++ test/index.js
@@ -2,61 +2,45 @@
 
 const {
     before,
-    after,
+    beforeEach,
+    afterEach,
     describe,
     it,
 } = require('mocha')
-const selenium = require('selenium-webdriver')
-const startServer = require('./utils/startServer')
-const expect = require('chai').use(require('chai-as-promised')).expect
-require('chromedriver')
-const until = require('selenium-webdriver/lib/until')
 
-const getPage = (driver, pageName) => {
-    return driver.get('http://127.0.0.1:1337/test/files/' + pageName + '.html')
-}
-
-const waitUntilHoneymateInitialized = (driver) => {
-    return driver.wait(until.elementLocated({ css: '.honey_ready' }), 20000)
-}
+const startServer = require('./utils/start-server')
+const { expect } = require('chai').use(require('chai-as-promised'))
+const Page = require('./utils/page')
 
 describe('Honeymate', () => {
-    let driver
+    let page
 
-    before(() => {
-        driver = new selenium.Builder()
-            .forBrowser('chrome')
-            .build()
-
-        startServer()
+    before(async () => {
+        await startServer()
     })
 
-    after(() => {
-        return driver.quit()
+    beforeEach(() => {
+        page = new Page()
+    })
+
+    afterEach(async () => {
+        await page.die()
     })
 
     describe('initialization', () => {
-        it('should initialize automatically', () => {
-            getPage(driver, 'initialization')
-            return waitUntilHoneymateInitialized(driver)
-                .then(() => driver.findElements({ css: '.honey' }))
-                .then((allHoneyNodes) => {
-                    return expect(driver.findElements({ css: '.honey_ready' })).to.eventually.have.lengthOf(allHoneyNodes.length)
-                })
+        it('should initialize automatically', async () => {
+            await page.init('initialization')
+            const allHoneyNodes = await page.find('.honey')
+            const readyNodes = await page.find('.honey_ready')
+            expect(readyNodes.length).to.equals(allHoneyNodes.length)
         })
 
-        it('should initialize manually', () => {
-            getPage(driver, 'manual-initialization')
-            return waitUntilHoneymateInitialized(driver)
-                .then(() => driver.executeScript(
-                    `const honeyNode = Honeymate.generateNode(
-                        document.querySelector('.nohoney')
-                    )`
-                ))
-                .then(() => driver.findElements({ css: '.nohoney' }))
-                .then((allHoneyNodes) => {
-                    return expect(driver.findElements({ css: '.honey_ready' })).to.eventually.have.lengthOf(allHoneyNodes.length + 1)
-                })
+        it('should initialize manually', async () => {
+            await page.init('manual-initialization')
+            await page.initManual('.nohoney')
+            const readyNodes = await page.find('.honey_ready')
+            const manualNodes = await page.find('.nohoney')
+            expect(readyNodes.length).to.equals(manualNodes.length + 1)
         })
     })
 })
---
+++ test/utils/page.js
@@ -0,0 +1,36 @@
+'use strict'
+const puppeteer = require('puppeteer')
+
+class Page {
+    async init (filename) {
+        this.browser = await puppeteer.launch()
+        this.page = await this.browser.newPage()
+        await this.page.goto(
+            `http://127.0.0.1:1337/test/files/${filename}.html`,
+            { waitUntil: 'load' }
+        )
+        await this.page.waitForSelector('.honey_ready')
+    }
+
+    async initManual (selector) {
+        await this.page.evaluate(
+            `const honeyNode = Honeymate.generateNode(
+                document.querySelector('${selector}')
+            )`
+        )
+    }
+
+    async die () {
+        await this.browser.close()
+    }
+
+    async findOne (selector) {
+        return await this.page.$$(selector)[0]
+    }
+
+    async find (selector) {
+        return await this.page.$$(selector)
+    }
+}
+
+module.exports = Page
