6954eec951887998d40e927b61b9bb8aa7ebc926
mishamyrt <misha@myrt.co>
2023-05-29 17:21:58 +0300

    WIP: Clean up

--- source/honeymate.js
+++
@@ -1,5 +0,0 @@
-import { Honeymate } from './index.mjs'
-
-window.Honeymate = Honeymate
-document.addEventListener('DOMContentLoaded', Honeymate.initiate)
-document.write('<style>.honey{opacity:0}@keyframes honeySpin{0%{transform:rotate(-360deg)}to{transform:rotate(360deg)}}</style>')
