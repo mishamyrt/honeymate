c387175c811b2838c3d3f40c73984a34ceebb531
mishamyrt <misha@myrt.co>
2023-06-02 00:50:43 +0300

    Add preview

--- preview/main.ts
+++ preview/main.ts
@@ -1,19 +1,33 @@
 import { animate, reset } from '../src'
 
-function expose (module: ParentNode = document): void {
-  animate(document, params => ({
-    ...params,
-    effect: 'zoom'
-  }))
+function expose (button?: HTMLButtonElement, node: ParentNode = document): void {
+  if (button) {
+    button.disabled = true
+  }
+  setTimeout(() => {
+    animate(node)
+      .then(() => {
+        if (button) {
+          button.disabled = false
+        }
+      })
+  }, 100)
 }
 
 expose()
 
-const button = document.querySelectorAll('button')
-button.forEach((b) => {
-  const node = b.parentElement as HTMLElement
-  b.onclick = () => {
-    reset(node)
-    expose(node)
+const modules = document.querySelectorAll<HTMLElement>('.module')
+modules.forEach(m => {
+  const button = m.querySelector<HTMLButtonElement>('button')
+  if (!button) {
+    return
+  }
+  const columns = m.querySelector<HTMLElement>('.columns')
+  if (!columns) {
+    return
+  }
+  button.onclick = () => {
+    reset(columns)
+    setTimeout(() => { expose(button, columns) }, 300)
   }
 })
